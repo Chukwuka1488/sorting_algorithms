8b91871bd99a203b24427047e505e0afd99c0bc7
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-09-25 13:11:18 -0700

    buble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ *               using the Bubble sort algorithm
+ * @array: array of integers to sort
+ * @size: number of elements in the array
+ *
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+	int swapped;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/* swap the elements */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
+
+				/* print the array */
+				print_array(array, size);
+			}
+		}
+
+		/* if no two elements were swapped by inner loop, then break */
+		if (swapped == 0)
+			break;
+	}
+}
